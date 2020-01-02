746602a5bb39d3e2e0ea17cc4cf96141b92a787b
Dee9975 <39603098+Dee9975@users.noreply.github.com>
2020-01-02 11:53:41 +0200

    Api changes

--- src/Application.kt
+++ src/Application.kt
@@ -17,17 +17,14 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 
-data class Snippet(val text: String)
 data class User(val name: String, val lastname: String, val number: Int, val position: String, val birthDate: String)
-
+data class PostUser(val name: PostUser.Text, val lastname: PostUser.Text, val number: PostUser.Text, val position: PostUser.Text, val birthDate: PostUser.Text) {
+    data class Text(val text: String)
+}
+data class Parameters(val id: Int)
 
 val client = KMongo.createClient()
 val database = client.getDatabase("hockey")
-
-val snippets = Collections.synchronizedList(mutableListOf(
-    Snippet("hello"),
-    Snippet("world")
-))
 
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
@@ -38,18 +35,36 @@ fun Application.module(testing: Boolean = false) {
     }
     val client = HttpClient(Apache) {
         routing {
-            get("/v2/getUsers") {
+            get("/api/v2/getUsers") {
                 val col = database.getCollection<User>("users")
-                val list: List<User> = col.find().toMutableList()
+                val limit: Int? = call.request.queryParameters["limit"]?.toInt()
+                val list: List<User> = col.find().limit(if(limit === null) 50 else limit).toMutableList()
                 val count: Int = list.count()
                 call.respond(mapOf("count" to count, "users" to list))
             }
-            get("/v2/getUser/") {
+            get("/api/v2/getUser/") {
                 val col = database.getCollection<User>("users")
-                val param1: String? = call.request.queryParameters["name"]
-                val list: List<User> = col.find(User::name eq param1).toMutableList()
+                val name: String? = call.request.queryParameters["name"]
+                val position: String? = call.request.queryParameters["position"]
+                val number: Int? = call.request.queryParameters["number"]?.toInt()
+                val limit: Int? = call.request.queryParameters["limit"]?.toInt()
+                val list: List<User> = col.find(or(User::name eq name, User::position eq position, User::number eq number)).limit(if(limit === null) 50 else limit).toMutableList()
                 val count: Int = list.count()
                 call.respond(mapOf("count" to count, "users" to list))
+            }
+            post("/api/v2/addUser") {
+                val post = call.receive<PostUser>()
+                val name: String = post.name.text
+                val lastname: String = post.lastname.text
+                val position: String = post.position.text
+                val number: Int = post.number.text.toInt()
+                val birthDate: String = post.birthDate.text
+                val col = database.getCollection<User>("users")
+                col.insertOne(User(name, lastname, number, position, birthDate))
+                call.respond(mapOf("OK" to true))
+            }
+            delete("/api/v2/deleteUser") {
+                val params = call.receive<PostUser>()
             }
         }
     }
